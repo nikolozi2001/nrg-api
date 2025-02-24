48a29f56148544db60c15b7777ac2765d923c621
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-02-24 15:53:08 +0400

    add route to serve README.md and update records endpoint

--- routes/records.js
+++ routes/records.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 const recordsController = require("../controllers/recordsController");
 const householdsController = require("../controllers/householdsController");
 const monthesController = require("../controllers/monthesController");
 const resourceController = require("../controllers/resourceController");
 
-router.get("/", recordsController.getAllRecords);
+router.get("/", (req, res) => {
+  const readmePath = path.join(__dirname, "../README.md");
+  fs.readFile(readmePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading README.md");
+    } else {
+      res.type("text/plain").send(data);
+    }
+  });
+});
+
+router.get("/records", recordsController.getAllRecords);
 router.get("/records/:id", recordsController.getRecordById);
 // router.post("/", recordsController.createRecord);
 // router.put("/:id", recordsController.updateRecord);
